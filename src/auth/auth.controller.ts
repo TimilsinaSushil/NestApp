306e866998b96842d0324a615718e7a98b6647a6
Sushil Timilsina <developer.timilsina@gmail.com>
2024-03-14 18:34:10 +0545

    User module complete

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,21 @@
-import { BadRequestException, Body, Controller, NotFoundException, Post } from '@nestjs/common';
+import { BadRequestException, Body, ClassSerializerInterceptor, Controller, Get, NotFoundException, Post, Req, Res, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs';
 import { RegisterDto } from './models/register.dto';
+import { JwtService } from '@nestjs/jwt';
+import {Request, Response} from 'express'
+import { AuthGuard } from './auth.guard';
 
+
+@UseInterceptors(ClassSerializerInterceptor)  //excludes attributes specified as exclude in model
 @Controller('')
 export class AuthController {
 
-    constructor(private userService:UserService){
+    constructor(
+        private userService:UserService,
+        private jwtService: JwtService
+        
+        ){
 
     }
 
@@ -23,7 +32,8 @@ export class AuthController {
     @Post('login')
     async login(
         @Body('email') email:string,
-        @Body('password') password:string
+        @Body('password') password:string,
+        @Res({passthrough:true}) response: Response  //to send generated cookie in response
     ){
         const user = await this.userService.findOne({where:{email}});
 
@@ -34,6 +44,27 @@ export class AuthController {
             throw new BadRequestException('Invalid credentials');
         }
 
+        const jwt = await this.jwtService.signAsync({id:user.id})
+        response.cookie('jwt',jwt,{httpOnly:true})
+
         return user;
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('user')
+    async user(@Req() request:Request){
+        const cookie = request.cookies['jwt']
+        const data = await this.jwtService.verifyAsync(cookie)
+        const user = this.userService.findOne({where:{id: data.id}})
+        return user;
+    }
+
+    @UseGuards(AuthGuard)
+    @Post('logout')
+    async logout(@Res({passthrough:true}) response: Response){
+        response.clearCookie('jwt')
+        return{
+            message:"success"
+        }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); //adds 'api' prefix to every route
   app.useGlobalPipes(new ValidationPipe()) //npm i --save class-validator class-transformer
+  app.use(cookieParser()); //For parsing cookies from client side
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials:true  //it means we will pass cookies back and fourth with every request
+  })
   await app.listen(3000);
 }
 bootstrap();
