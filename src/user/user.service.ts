1abdc4de8c5f3fbb0bc09f1aaf695e8569221b4c
Sushil Timilsina <developer.timilsina@gmail.com>
2024-03-20 14:06:04 +0545

    Roles crud functionality completed

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,7 +6,8 @@ import { User } from './models/user.entitity';
 @Injectable()
 export class UserService {
     constructor(
-        @InjectRepository(User) private readonly userRepository: Repository<User>
+        @InjectRepository(User) 
+        private readonly userRepository: Repository<User>
     ){
     }
 
