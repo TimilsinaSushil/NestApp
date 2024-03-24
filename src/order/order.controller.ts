2ae54330dda2207f78866b663325d5fd1080565c
Sushil Timilsina <developer.timilsina@gmail.com>
2024-03-24 15:13:26 +0545

    csv export complete

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,6 +1,10 @@
-import { ClassSerializerInterceptor, Controller, Get, Query, UseGuards, UseInterceptors } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller, Get, Post, Query, Res, UseGuards, UseInterceptors } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { AuthGuard } from 'src/auth/auth.guard';
+import {Response} from 'express'
+import {Parser} from "json2csv"
+import { Order } from './models/order.entity';
+import { OrderItem } from './models/order-item.entity';
 
 @UseGuards(AuthGuard)
 @UseInterceptors(ClassSerializerInterceptor)
@@ -13,5 +17,40 @@ export class OrderController {
     @Get('orders')
     async all(@Query('page') page:number=1){
         return this.orderService.paginate(page, ['order_items']);
+    }
+
+    @Post('export')
+    async export(@Res() res:Response){
+            const parser = new Parser({
+                fields: ['ID', 'Name','Product Title', 'Price','Quantity']
+            })
+
+            const orders = await this.orderService.all(['order_items'])
+
+            const json = [];
+
+            orders.forEach((o:Order)=>{
+                json.push({
+                    ID: o.id,
+                    Name: o.name,
+                    'Product Title':'',
+                    Price:'',
+                    Quantity:''
+                });
+                o.order_items.forEach((i:OrderItem)=>{
+                    json.push({
+                    ID: '',
+                    Name: '',
+                    'Product Title':i.product_title,
+                    Price: i.price,
+                    Quantity: i.quantity
+                    });
+                })
+            });
+            
+            const csv =parser.parse(json)
+            res.header('Content-Type', 'text/csv')
+            res.attachment('orders.csv');
+            return res.send(csv)
     }
 }
